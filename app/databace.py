1030cdda8745e20cf6f7c637eae335530f917fc2
omon212 <omonnuloraimkulov@gmail.com>
2025-01-27 15:55:15 +0500

    edit requirements and db codes

--- app/databace.py
+++ app/databace.py
@@ -2,10 +2,8 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
 async def verify_pass(plain_pass: str, hashed_pass: str):
     return pwd_context.verify(plain_pass, hashed_pass)
-
 
 async def auth_user(username: str, password: str):
     user = db.get(username)
@@ -14,7 +12,6 @@ async def auth_user(username: str, password: str):
     if not await verify_pass(password, user["password"]):
         return None
     return user
-
 
 db = {
     "omon212": {
